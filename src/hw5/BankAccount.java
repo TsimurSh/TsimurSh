3dc160b06631bcc7e70b7a3a1ea8d6c6698fd066
Tsimur <nut.t@icloud.com>
2021-10-31 15:02:39 +0100

    lessone 5

--- src/hw5/BankAccount.java
+++ src/hw5/BankAccount.java
@@ -3,6 +3,16 @@ package hw5;
 public class BankAccount {
     int id = 11;
     String name = "Timur";
-    double balance = 1000;
+    double balance = 100.00;
+
+    double popolnienieScheta (double dollars) {
+        balance += dollars;
+        return balance;
+    }
+
+    double sniatieSoScheta (double dollars) {
+        balance -= dollars;
+        return balance;
+    }
 
 }

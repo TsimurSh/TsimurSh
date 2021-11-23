40f7f6dd18cdfe8bbc37fcf44613c865eb89d71b
Tsimur <nut.t@icloud.com>
2021-11-23 23:50:39 +0100

    final non-acces modifier

---
+++ src/main/java/lessone8/Finalnonaccess.java
@@ -0,0 +1,31 @@
+package lessone8;
+
+public class Finalnonaccess {
+
+    public final int a;
+
+    public Finalnonaccess() {
+        a=15;
+    }
+
+    public Finalnonaccess(boolean b) {
+        a = 10;
+    }
+
+    /*int wdwa ();
+    wdwa = a*2; // нельзя менять при final non-access modifier
+    return wdwa;*/
+
+}
+
+public class Human {
+
+    String name = "Alex";
+    short age = 46;
+       final Finalnonaccess f = new Finalnonaccess();
+    public static void main(String[] args) {
+        Human h = new Human();
+        h.f = new Finalnonaccess(); // новый обьект с новым адресом
+
+    }
+}

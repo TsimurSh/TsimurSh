c933fb1b15140c22afc244429caf7042a4188571
Tsimur <nut.t@icloud.com>
2021-11-28 15:30:29 +0100

    Array memorization

--- src/main/java/array/Lesson18_Array.java
+++ src/main/java/array/Lesson18_Array.java
@@ -1,12 +1,19 @@
 package array;
 
 public class Lesson18_Array {
+    public static void main(String[] args) {
 
-    int [] array1;
-    String [] array2;
-    double [][] array3;
-
-    array1 = new int [0];
-    array2 = new String [3];
-    array3 = new
+        // decloration
+        int[] array1;
+        String[] array2;
+        double[][] array3;
+        int[][] array4;
+        // allocation
+        array1 = new int[0];
+        array2 = new String[3];
+        array3 = new[4][2]; // 4 одномерных массива по два элемента
+        array4 = new[3][];  // с разным количеством переменных
+        // initialization
+        System.out.println(array2.length);
+    }
 }

ecbee249c42d514a417c75750d2d1691c6808171
Tsimur <nut.t@icloud.com>
2021-11-28 13:54:41 +0100

    Array memorization

---
+++ src/main/java/array/Lesson18_Array.java
@@ -0,0 +1,12 @@
+package array;
+
+public class Lesson18_Array {
+
+    int [] array1;
+    String [] array2;
+    double [][] array3;
+
+    array1 = new int [0];
+    array2 = new String [3];
+    array3 = new
+}

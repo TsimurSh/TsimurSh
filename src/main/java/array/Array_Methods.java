a8e82a891507268706eb3fcf9c961bc4219b2f88
Tsimur <nut.t@icloud.com>
2021-11-29 14:20:14 +0100

    Array memorization dinamic initialization

---
+++ src/main/java/array/Array_Methods.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Arrays;
+
+public class Array_Methods {
+    public static void main(String[] args) {
+        int arr1[] = {0, -4, 7, 12, -50, 101};
+        Arrays.sort(arr1); // соритирует по возрастанию
+
+        int index1 = Arrays.binarySearch(arr1,7); // ищет только отсортированные элементы массивов
+        System.out.println(index1);
+
+    }
+
+    public static void minMax (double [] array) {
+      double max = array [0];
+      double min = array [0];
+        for (int i = 1; i < array.length; i++) {
+            if  (array[i] > max)
+                max = array[i];
+            if (array[i] < min)
+               min =  array[i];
+        }
+
+
+    }
+
+}

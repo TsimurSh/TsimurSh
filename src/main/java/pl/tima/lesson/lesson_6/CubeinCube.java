ace6079e78f3698f2e3d1ae449b395059b08c29c
Tsimur <nut.t@icloud.com>
2021-12-07 21:54:23 +0100

    cube*cube

---
+++ src/main/java/pl/tima/lesson/lesson_6/CubeinCube.java
@@ -0,0 +1,15 @@
+package pl.tima.lesson.lesson_6;
+
+public class CubeinCube {
+
+    public static void main(String[] args) {
+        System.err.println(ningth(3));
+    }
+
+    public static long cube (long a) {
+        return a*a*a;
+    }
+    public static long ningth (long b) {
+        return cube (cube(b));
+    }
+}

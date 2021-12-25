5cae42be62e05138de17592f94f08ca05ed1cf11
Tsimur <nut.t@icloud.com>
2021-12-25 12:20:11 +0100

    lesson18

---
+++ src/main/java/pl/tima/lesson/lesson_16_String/ParselToWord.java
@@ -0,0 +1,13 @@
+package pl.tima.lesson.lesson_16_String;
+
+public class ParselToWord {
+        public static void main(String[] args) {
+            String string = "Кто желает поработать?";
+            int getWordCount = getWordCount(string);
+            System.out.println("Количество слов в строке \"" + string + "\" равно " + getWordCount);
+        }
+
+        public static int getWordCount(String string) {
+            return string.split(" ").length;
+        }
+    }

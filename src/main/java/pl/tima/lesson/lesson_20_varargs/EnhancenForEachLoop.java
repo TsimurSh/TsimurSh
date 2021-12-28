ab3d5d80310d68dd71e5ba73a05b0eb69dab822f
Tsimur <nut.t@icloud.com>
2021-12-28 20:11:24 +0100

    HOMEwork for each (String [] ... a) обработка принятых массивов через varargs

--- src/main/java/pl/tima/lesson/lesson_20_varargs/EnhancenForEachLoop.java
+++ src/main/java/pl/tima/lesson/lesson_20_varargs/EnhancenForEachLoop.java
@@ -3,15 +3,27 @@ package pl.tima.lesson.lesson_20_varargs;
 public class EnhancenForEachLoop {
 
     static int  array [] = new int[]{2,45,6,987,42,45,7,3};
+    static String arraS [] = new String []{"3","45","6","987","42","45","7","3"};
 
     public static void main(String[] args) {
-        for (int a : array) {
+        F1D: for (int a : array) {
             System.out.printf(a + " ");
+        }
+        int  array2 [][] = new int[][]{{2,45,6,},{987,42,4},{5,7,3}};
+        F2D: for ( int [] arr:array2){
+            for (int a : arr){
+                System.out.print(a + " ");
+            }
         }
         int sumArr = 0;
         for ( int temp : array){
             sumArr += temp;
         }
-        System.out.println(sumArr);
+
+        for (int a : array){
+            for (String b : arraS){
+                System.out.print(a + " " + b);
+            }
+        }
     }
 }

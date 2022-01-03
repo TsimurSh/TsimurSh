46781232309896e6fc3c30badcb52b55822eaa77
Tsimur <nut.t@icloud.com>
2022-01-03 22:39:51 +0100

    Collection Set HashSet

--- src/main/java/pl/tima/lesson/lesson_22/CollectionsExample.java
+++ src/main/java/pl/tima/lesson/lesson_22/CollectionsExample.java
@@ -1,11 +1,10 @@
 package pl.tima.lesson.lesson_22;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class CollectionsExample {
     public static void main(String[] args) {
-
+        Set <Integer> intSet = new HashSet<>();
         Map<Integer ,String> map1 = new HashMap<>();
         map1.put(881, "surname Shichsefiyeu");
         map1.put(882, "Christian name");
@@ -15,5 +14,13 @@ public class CollectionsExample {
         map1.remove(885);
         System.out.println(map1);
 
+        get10000Elements((List) intSet);
+
+    }
+    public static void get10000Elements (List list){
+        int count = 10000;
+        for (int i = 0; i < count; i++){
+            list.get(count /=2);
+        }
     }
 }

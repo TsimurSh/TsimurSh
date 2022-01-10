aaffc6fe9ed440e928b0b3e20a6fcdc863f8d46a
Tsimur <nut.t@icloud.com>
2022-01-10 21:52:37 +0100

    edited

--- src/main/java/pl/tima/lesson/lesson_19_arrays/ArrayMethods.java
+++ src/main/java/pl/tima/lesson/lesson_19_arrays/ArrayMethods.java
@@ -4,64 +4,71 @@ import java.util.Arrays;
 
 public class ArrayMethods {
 
-    public static void bubbleSort (int [] arr){
+    public static void bubbleSort(int[] arr) {
         boolean sort = false;
         int temp;
-        while (!sort){
+        while (!sort) {
             sort = true;
-            for (int i = 0; i < arr.length - 1 ; i++) {
-                if (arr[i] > arr[i+1]){
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
                     temp = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i+1] = temp;
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
                     sort = false;
                 }
             }
         }
     }
 
-    public static void main(String[] args) {
+    public static boolean main(String[] args) {
         int arr1[] = {0, -4, 7, 12, -50, 101};
-        int arr2[] = {0, -4, 7, 12, -50, 101};
-//        int arr2[] = {0, -4, 7, 7, 5, 11};
+        String arr2[] = {"to take keys alont with somebody", "876", "to hamper", "foreign exchange desk"};
+        int arr8[] = {0, -4, 7, 7, 5, 11};
         bubbleSort(arr1);
         System.out.println(Arrays.toString(arr1));
         Arrays.sort(arr1); // соритирует по возрастанию
 
         int index1 = Arrays.binarySearch(arr1, 7); // ищет только отсортированные элементы массивов
-//        System.out.println(index1);
+        System.out.println(index1);
 
-        int boo = Arrays.compare(arr1,arr2);
-        int bo = Arrays.mismatch(arr1,arr2);
-        System.out.println(boo + " "+ bo);
-    }
+        int boo = Arrays.compare(arr1, arr8);
+        int bo = Arrays.mismatch(arr1, arr8);
+        System.out.println(boo + " " + bo);
 
-    public static void minMax(double[] array) {
-        double max = array[0];
-        double min = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > max)
-                max = array[i];
-            if (array[i] < min)
-                min = array[i];
-        }
-        class ArrayFill {
 
-            public int[] array = new int[20];
-            public  int valueStart = 10;
-            public  int valueEnd = 13;
+        int[] array1 = new int[20];
+        int valueStart = 10;
+        int valueEnd = 13;
+        double [] digits = new double[]{154.23467,89.7867553,3243.5678,8.76542,23.4565,67.87,898.676,34.5657897};
 
-            public  void main(String[] args) {
 
-                int fPart = array.length % 2 == 0 ? array.length / 2 : array.length / 2 + 1;
-                Arrays.fill(array, 0, fPart, valueStart);
+        int fPart = array1.length % 2 == 0 ? array1.length / 2 : array1.length / 2 + 1;
+        Arrays.fill(array1, 0, fPart, valueStart);
 
-                Arrays.fill(array, fPart, array.length, valueEnd);
-                System.out.println(Arrays.toString(array));
+        Arrays.fill(array1, fPart, array1.length, valueEnd);
+        System.out.println(Arrays.toString(array1));
+
+        public int num = 0;
+        public static boolean isNumb (String str){ // chek character
+            if (str.length() == 0) {
+                return false;
+            }
+
+            char[] chars = str.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                char character = chars[i];
+                if ((i != 0 && character == '-') // Строка содержит '-'
+                        || (!Character.isDigit(character) && character != '-') // или не цифра и не начинается с '-'
+                        || (chars.length == 1 && character == '-')) // или одиночный '-'
+                {
+                    return false;
+                }
             }
         }
-
-
+        for (String strings : arr2){
+           if( isNumb(arr2) ){
+             int num +=  Integer.parseInt(strings);
+           }
+        }
     }
-
 }

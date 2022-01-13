0a84c6f5bf60de7f58c00edcf13e86e974b12fa2
Tsimur <nut.t@icloud.com>
2022-01-13 09:22:09 +0100

    HW22

--- src/main/java/pl/tima/homework/homework22/Student.java
+++ src/main/java/pl/tima/homework/homework22/Student.java
@@ -1,35 +1,25 @@
 package pl.tima.homework.homework22;
 
 public class Student {
-    StringBuilder name;
-    byte course;
-    float grade;
-    public static char[][] chars = {
-                {'a', 'b', 'c', 'd', 'e', 'f'},
-                {'g', 'h', 'i', 'j', 'k', 'l'},
-                {'m', 'n', 'o', 'p', 'q', 'r'},
-                {'s', 't', 'u', 'v', 'w', 'x'}};
+    private StringBuilder name;
+    private byte course;
+    private float grade;
 
-        public static void main(String[] args) {
-            printArray();
-
-            for (int i =0; i < chars.length; i++){
-                for (int j =0; j <chars[i].length; j++){
-                    if ( i != 0 && j != 0 &&  i != 3 && j != 5)
-                        chars [i][j] = '-';
-                }
-            }
-
-            printArray();
-        }
-
-        private static void printArray() {
-            for (int i = 0; i < chars.length; i++) {
-                for (int j = 0; j < chars[i].length; j++) {
-                    System.out.print(chars[i][j] + " ");
-                }
-                System.out.println();
-            }
-            System.out.println();
-        }
+    public void setName(StringBuilder in) {
+        if (in.length() > 0 && in.length() < 3)
+            this.name = in;
     }
+
+    public void setGrade(float grade) {
+        if (grade > 0 && grade <= 10)
+            this.grade = grade;
+    }
+
+    public void setCourse(byte course) {
+        if (course > 0 && course <= 4)
+            this.course = course;
+    }
+    public void showInfo (){
+        System.out.println("Имя: " + name.toString() + "Курс: " + course + "Оценка: " + grade);
+    }
+}

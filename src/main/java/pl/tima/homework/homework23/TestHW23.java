a018c8ce47cdbfcd53ba6ac8aa5af95477432b19
Tsimur <nut.t@icloud.com>
2022-01-21 10:12:36 +0100

    HW24

---
+++ src/main/java/pl/tima/homework/homework23/TestHW23.java
@@ -0,0 +1,15 @@
+package pl.tima.homework.homework23;
+
+public class TestHW23 {
+//    public static void abc (InheritanceX, InheritanceY){
+//        System.out.println("privat");
+//    }
+//    public static void abc (InheritanceY, InheritanceX){
+//        System.out.println("poke");
+//    }
+//
+//    public static void main(String[] args) {
+//        InheritanceX yy = new InheritanceY();
+//        System.out.println();
+//    }
+}
---
+++ src/main/java/pl/tima/homework/homework24/Human.java
@@ -0,0 +1,4 @@
+package pl.tima.homework.homework24;
+
+public class Human {
+}

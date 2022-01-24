677776a5eb04f86cf3406aa5ed78aa0bd62b4e71
Tsimur <nut.t@icloud.com>
2022-01-24 23:52:18 +0100

    HW24 done

--- src/main/java/pl/tima/facultativ/repository/UserRepository.java
+++ src/main/java/pl/tima/facultativ/repository/UserRepository.java
@@ -10,7 +10,7 @@ public class UserRepository  {
     public static List<User> getUsersFromDataBase() {
         User user1 = new User(1, "Wlad", "Bumaga", 32, Gender.MALE);
         User user2 = new User(3, "Olga", "Buaga", 30, Gender.FEMALE);
-        User user3 = new User(7, "Lad", "Buli", 23, Gender.MALE);
+        User user3 = new User(7, "Lada", "Buli", 23, Gender.MALE);
 
         List <User> users = new ArrayList<>();
         users.add(user1);
--- src/main/java/pl/tima/homework/homework22/TestAnimals.java
+++ src/main/java/pl/tima/homework/homework22/TestAnimals.java
@@ -1,7 +1,9 @@
 package pl.tima.homework.homework22;
 
+import java.io.IOException;
+
 public class TestAnimals {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Cat cat = new Cat("Vasilij");
         Dog dog = new Dog("Bobyk");
         cat.sleep();

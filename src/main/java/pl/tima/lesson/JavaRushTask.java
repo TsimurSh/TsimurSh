13d5a83f5eb0ceff4b1b9474547c2133228249d8
Tsimur <nut.t@icloud.com>
2022-01-25 23:35:46 +0100

    FileInputSteram

---
+++ src/main/java/pl/tima/lesson/JavaRushTask.java
@@ -0,0 +1,32 @@
+package pl.tima.lesson;
+
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+/*
+Писатель в файл с консоли
+*/
+
+public class JavaRushTask {
+        public static void main(String[] args) throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String sours = reader.readLine();
+            BufferedWriter newWriter = new BufferedWriter(new FileWriter(sours));
+            StringBuilder string = new StringBuilder();
+
+            String temp = "";
+            while (!temp.equals("exit")){
+                temp = reader.readLine();
+                string.append(temp).append('\n');
+            }
+            newWriter.write(string.toString());
+            newWriter.close();
+        }
+    }
+
+
+

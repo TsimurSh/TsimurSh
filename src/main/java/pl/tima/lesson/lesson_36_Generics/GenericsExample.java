7e78a391396050f499002f3e59a2735c708c559e
Tsimur <nut.t@icloud.com>
2022-02-07 23:00:49 +0100

    Generics Exempl

--- src/main/java/pl/tima/lesson/lesson_36_Generics/GenericsExample.java
+++ src/main/java/pl/tima/lesson/lesson_36_Generics/GenericsExample.java
@@ -1,9 +1,15 @@
 package pl.tima.lesson.lesson_36_Generics;
 
-public class GenericsExmple {
+import java.util.ArrayList;
+
+public class GenericsExample {
     public static void main(String[] args) {
     Info <String> tipe = new Info<String>("No-noooo");
-
+    ArrayList <Integer> intList = new ArrayList<>();
+    intList.add(45);
+    intList.add(43);
+    intList.add(44);
+        System.out.println(Info.getElementFromList(intList));
     }
     static class Info <T> {
         private T value;
@@ -12,6 +18,9 @@ public class GenericsExmple {
         }
         public String toString (){
             return " [(" + value +  ")]";
+        }
+        public static  <T> T getElementFromList(ArrayList <T> list){
+            return list.get(1);
         }
     }
 }

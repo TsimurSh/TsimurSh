f44db8b5477f4135e3327f8e809fde726a699f5f
Tsimur <nut.t@icloud.com>
2022-02-08 22:35:14 +0100

    Override methods

---
+++ src/main/java/pl/tima/lesson/lesson_26_OverrideEqualsToString/Test.java
@@ -0,0 +1,31 @@
+package pl.tima.lesson.lesson_26_OverrideEqualsToString;
+
+public class Test {
+    String color;
+    String model;
+
+    public Test(String color, String model) {
+        this.color = color;
+        this.model = model;
+    }
+
+    public static void main(String[] args) {
+        Test test1 = new Test("Green", "POTR");
+        Test test3 = new Test("Green", "POMM");
+        Test test2 = new Test("Green", "POMM");
+        System.out.println(test2.toString(test3));
+        System.out.println();
+    }
+    public  String toString(Object o) {
+    return "Объект Test имеет поля: " + this.color +" и " + this.model;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Test) {
+            Test test = (Test) o;
+            if (o == null || getClass() != o.getClass()) return false;
+            return color.equals(test.color) && model.equals(test.model);
+        }
+        return false;
+    }
+}

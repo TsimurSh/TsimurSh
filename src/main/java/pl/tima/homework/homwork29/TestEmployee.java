1869dbb0de5602c9c5905c37a8726381ed6494bb
Tsimur <nut.t@icloud.com>
2022-02-19 00:39:32 +0100

    HW 29

--- src/main/java/pl/tima/homework/homwork29/TestEmployee.java
+++ src/main/java/pl/tima/homework/homwork29/TestEmployee.java
@@ -1,7 +1,6 @@
 package pl.tima.homework.homwork29;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Predicate;
 
 public class TestEmployee {
@@ -21,16 +20,18 @@ public class TestEmployee {
     public static void main(String[] args) {
         Emloyee empl3 = new Emloyee("TRIK", "IT", 3450);
 
-        List <Emloyee> listOFEmp = new ArrayList<>();
+        ArrayList<Emloyee> listOFEmp = new ArrayList<>();
         var test1 = new TestEmployee(); // variable type inference
         listOFEmp.add(new Emloyee("Grisha", "Fishers", 2980));
         listOFEmp.add(new Emloyee("Grosha", "GrolschLover", 3980));
         listOFEmp.add(new Emloyee("Grasha", "IT", 4980));
         listOFEmp.add(new Emloyee("Gresha", "Builders", 5980));
 
-//        test1.filtrationEmployee((Emloyee empl) -> {empl.department.equals("IT"); empl.salary > 200;});
-//        test1.filtrationEmployee((Emloyee empl) -> {empl.name.equals(empl.department);});
-//        test1.filtrationEmployee((Emloyee empl) -> {empl.name.startsWith("A"); empl.salary < 450;});
+        test1.filtrationEmployee(listOFEmp, empl -> empl.department.equals("IT") && empl.salary > 200);
+        test1.filtrationEmployee(listOFEmp, empl -> {
+            return empl.name.equals(empl.department);
+        });
+        test1.filtrationEmployee(listOFEmp, empl -> empl.name.startsWith("A") && empl.salary < 450);
         listOFEmp.removeIf(emp -> emp.name.equals("Grisha"));
         System.out.println(listOFEmp);
     }

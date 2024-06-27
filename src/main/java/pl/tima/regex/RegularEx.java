0d0b7d4468d84fcf2337c5ea247bf7bd5a53a49c
Timur Shykhsefiyeu <nut.t@icloud.com>
2024-06-27 12:28:51 +0200

    ip regex check

--- src/main/java/pl/tima/regex/RegularEx.java
+++ src/main/java/pl/tima/regex/RegularEx.java
@@ -7,6 +7,7 @@ public class RegularEx {
 
     public static void main(String[] args) {
         String testString = "ё(@(@)J71490ўўў1304!@fhfykg,#$%^&*(T)";
+        String testIP = "255.155.67.99";
         Pattern pattern1 = Pattern.compile("\\w+"); // Цельные последовательные цифры и буквы
         Pattern pattern2 = Pattern.compile("\\W");
         Pattern pattern3 = Pattern.compile("\\D");
@@ -15,9 +16,15 @@ public class RegularEx {
         Pattern pattern = Pattern.compile("\\w+(9)");
         Matcher matcher = pattern4.matcher(testString);
 
-        while (matcher.find()) {
-            System.out.println( " " + matcher.start()+ " : " + matcher.group());
-        }
+        Pattern patternIP = Pattern.compile("(25[0-5] | 2[0-4]\\d | [01]?\\d?\\d)(\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d?\\d)");
+        Matcher matcherIP = patternIP.matcher(testIP);
+        System.out.println(matcherIP.matches());
+
+
+
+//        while (matcher.find()) {
+//            System.out.println( " " + matcher.start()+ " : " + matcher.group());
+//        }
 
     }
 }
